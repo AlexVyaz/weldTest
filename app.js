64ba086566774f95d9b9997e24920ffca21336d4
AlexVyaz <alex.vyazovcev@mail.ru>
2020-09-01 21:43:28 +0500

    xz

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-function rez () {
-    let = k1, k2, rezult;
-    let k1 = document.getElementById('k1');
-    console.log(k1)
+function umn() {
+    let = k1, k2;
+    k1 = document.getElementById('k1').value;
+    k1 = parseFloat(k1);
+
+    k2 = document.getElementById('k2').value;
+    k2 = parseFloat(k2);
+
+    result = k1 * k2;
+
+    document.getElementById('rez').innerHTML = result;
 }
-console.log(k1)
